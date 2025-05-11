a5e74aaa9fef417b9d4d3955670a144b05419220
tuffstuff9 <hamzawaheed057@gmail.com>
2025-05-11 17:23:10 +0500

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  base: "/app/",
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'robots.txt', 'apple-icon.png'],
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "robots.txt", "apple-icon.png"],
       manifest: {
-        name: 'MindScribe Journal App',
-        short_name: 'MindScribe',
-        description: 'Journal app to capture your thoughts',
-        theme_color: '#ffffff',
+        name: "MindScribe Journal App",
+        short_name: "MindScribe",
+        description: "Journal app to capture your thoughts",
+        theme_color: "#ffffff",
         icons: [
           {
-            src: 'android-icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
+            src: "android-icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
   ],
-})
+});
